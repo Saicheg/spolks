7d42c06f768a6c500a78b634afeebeb281abcd5f
keritaf <keritaf@gmail.com>
2012-11-23 18:16:59 +0300

    Lab07: implement threading for tcp

--- lab07/server.c
+++ lab07/server.c
@@ -5,14 +5,15 @@
 #include <sys/sendfile.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <pthread.h>
 #include "sockutils.h"
 #include "common.h"
 
 void tcp_server(int sd);
+void* tcp_server_thread(void * desc);
 void udp_server(int sd);
 
 char *host, *service, *proto, *filename;
-FILE* fd;
 struct sockaddr_in server_addr, remote_addr;
 socklen_t rlen;
 
@@ -48,10 +49,9 @@ int main(int argc, char* argv[]) {
 }
 
 void tcp_server(int sd) {
-
-  off_t offset = 0;          /* file offset */
   int desc;
-  char buf[BUF_SIZE];
+  pthread_t th;
+  //pthread_attr_t ta;
 
   while(1) {
     rlen = sizeof(struct sockaddr_in);
@@ -60,57 +60,64 @@ void tcp_server(int sd) {
       perror("\nConnection error: ");
       continue;
     }
-
-    // Read file start offset
-    if(read(desc, buf, sizeof(buf)*sizeof(buf[0])) > 0 ) {
-      offset = atoi(buf);
-      printf("Requested start offset: %lu\n", offset);
-    } else {
-      perror("\nError getting start file offset: ");
-      close(desc);
-      fclose(fd);
-      continue;
-    }
-
-    fd = fopen(filename, "r");
-    if(fd == NULL) {
-      perror("\nError opening file: ");
-      close(sd);
-      exit(EXIT_FAILURE);
-    }
-    fseek(fd, offset, SEEK_SET);
-
-
-    size_t bytes_total = 0;
-    printf("Start sending file %s\n", filename);
-    while(1){
-      size_t bytes_read = fread(buf, sizeof(buf[0]), sizeof(buf), fd);
-      size_t bytes_sent = send(desc, buf, bytes_read, 0);
-      if (bytes_sent == -1) {
-        perror("\nError writing to socket: ");
-        break;
-      }
-
-      bytes_total += bytes_sent;
-      printf("Sent last/total: %zd/%zd\n", bytes_sent, bytes_total);
-
-      /* bytes_sent = send(desc, oob_message, 1, MSG_OOB); */
-      /* if (bytes_sent == -1) */
-      /* { */
-      /*   perror("\nError sending out-of-band data: "); */
-      /* } */
-
-
-      if(feof(fd)) {
-        printf("End sending file %s\n", filename);
-        break;
-      }
-    }
-    /* send(desc, oob_message, 1, MSG_OOB); */
-    shutdown(desc, SHUT_RDWR);
-    fclose(fd); // file descriptor
-    close(desc); // client descriptor
+    pthread_create(&th, NULL, tcp_server_thread, &desc);
   }
+}
+
+void* tcp_server_thread(void* desk){
+  int rsd = *(int*)desk;
+  printf("THREAD: Created thread %zd\n", pthread_self());
+  off_t offset = 0;  //file offset
+  FILE* fd;
+  char buf[BUF_SIZE];
+
+  // Read file start offset
+  if(read(rsd, buf, sizeof(buf)*sizeof(buf[0])) > 0 ) {
+    offset = atoi(buf);
+    printf("Requested start offset: %lu\n", offset);
+  } else {
+    perror("\nError getting start file offset: ");
+    close(rsd);
+    return NULL;
+  }
+
+  fd = fopen(filename, "r");
+  if(fd == NULL) {
+    perror("\nError opening file: ");
+    return NULL;
+  }
+  fseek(fd, offset, SEEK_SET);
+
+
+  size_t bytes_total = 0;
+  printf("Start sending file %s\n", filename);
+  while(1){
+    size_t bytes_read = fread(buf, sizeof(buf[0]), sizeof(buf), fd);
+    size_t bytes_sent = send(rsd, buf, bytes_read, 0);
+    if (bytes_sent == -1) {
+      perror("\nError writing to socket: ");
+      break;
+    }
+
+    bytes_total += bytes_sent;
+    //printf("Sent last/total: %zd/%zd\n", bytes_sent, bytes_total);
+
+    /* bytes_sent = send(desc, oob_message, 1, MSG_OOB); */
+    /* if (bytes_sent == -1) */
+    /* { */
+    /*   perror("\nError sending out-of-band data: "); */
+    /* } */
+
+    if(feof(fd)) {
+      printf("End sending file %s\n", filename);
+      break;
+    }
+  }
+  /* send(desc, oob_message, 1, MSG_OOB); */
+  shutdown(rsd, SHUT_RDWR);
+  fclose(fd); // file descriptor
+  close(rsd); // client descriptor
+  return NULL;
 }
 
 void udp_server(int sd) {
@@ -121,6 +128,7 @@ void udp_server(int sd) {
   struct stat st;
   long long size;
   size_t bytes_read, bytes_sent;
+  FILE* fd;
 
   fd = fopen(filename, "r");
   if(fd == NULL) {
