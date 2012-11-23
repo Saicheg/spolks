b679a28703dd0eb7d5365ed4818fc949e78d833e
keritaf <keritaf@gmail.com>
2012-11-23 19:14:22 +0300

    Lab07: forgot to add common.h

--- lab07/common.h
+++ lab07/common.h
@@ -1,9 +1,22 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#define BUF_SIZE 128
+#include <sys/types.h>
+
+#define BUF_SIZE 1024
+
+
 
 char *oob_message = "~";
 char *end_message = "e_o_f";
+
+struct request_udp {
+	int rsock;
+	socklen_t rlen;
+	struct sockaddr_in raddr;
+	char rbuf[BUF_SIZE];
+	size_t rbuflen;
+
+};
 
 #endif
