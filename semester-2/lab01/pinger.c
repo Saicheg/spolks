ce01326e2864ac07851827eaeb3a760cadf5f67b
keritaf <keritaf@gmail.com>
2013-02-19 11:51:40 +0300

    Adding source and destination address parsing Signed-off-by:keritaf <keritaf@gmail.com>

--- semester-2/lab01/pinger.c
+++ semester-2/lab01/pinger.c
@@ -1,18 +1,34 @@
+#define _BSD_SOURCE
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <netinet/ip_icmp.h>
+#include <signal.h>
 #include <argtable2.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/ip_icmp.h>
+#include <netdb.h>
+
+#include "pinger.h"
 
 #define PROGRAM_NAME "pinger"
 
-int main_pinger(const char* source_address, const char* destination_address, int ttl, int listen, int continuous);
+int main_pinger(const char* source_address, const char* destination_host_name, int ttl, int listen);
+void main_catcher();
+
+int pid;
+int catcher_listen_all;
 
 int main(int argc, char ** argv) {
-    struct arg_str *argument_source_ip = arg_str0("s",
-                                                  "source-ip",
-                                                  "<ip>",
-                                                  "Source address, specified in IP header of echo request");
+    struct arg_str *argument_address_source = arg_str0("s",
+                                                       "source",
+                                                       "<source-address>",
+                                                       "Source address, specified in IP header of echo request");
     struct arg_int *argument_ttl = arg_int0("t",
                                             "ttl",
                                             "<1-255>",
@@ -23,35 +39,29 @@ int main(int argc, char ** argv) {
         *argument_help = arg_lit0("h",
                                   "help",
                                   "Show help");
-    struct arg_str *argument_destination_address = arg_str1(NULL,
+    struct arg_str *argument_address_destination = arg_str1(NULL,
                                                             NULL,
                                                             "<destination-address>",
                                                             "Address to ping.");
-
-    struct arg_lit *argument_continuous = arg_lit0("c",
-                                                   "continuous",
-                                                   "Send requests continuously.");
-
     struct arg_end *argument_end = arg_end(20);
 
 
     void* arguments_table[] = {
                                argument_listen,
-                               argument_continuous,
-                               argument_source_ip,
+                               argument_address_source,
                                argument_ttl,
-                               argument_destination_address,
+                               argument_address_destination,
                                argument_help,
                                argument_end
     };
 
     if (arg_nullcheck(arguments_table) != 0) {
         printf("%s: insufficient memory\n", PROGRAM_NAME);
-        exit(1);
+        exit(PINGER_EXITERROR_PARAMETERS);
     }
 
     argument_ttl->ival[0] = 255;
-    argument_source_ip->sval[0] = NULL;
+    argument_address_source->sval[0] = NULL;
 
     int parse_errors_count = arg_parse(argc, argv, arguments_table);
 
@@ -59,36 +69,34 @@ int main(int argc, char ** argv) {
         printf("Usage: %s", PROGRAM_NAME);
         arg_print_syntax(stdout, arguments_table, "\n");
         printf("Try using %s --help for more information.\n", PROGRAM_NAME);
-        exit(0);
+        exit(EXIT_SUCCESS);
     }
 
     if (argument_help->count > 0) {
         printf("Usage: %s", PROGRAM_NAME);
         arg_print_syntax(stdout, arguments_table, "\n");
         arg_print_glossary_gnu(stdout, arguments_table);
-        exit(0);
+        exit(EXIT_SUCCESS);
     }
 
     if (parse_errors_count > 0) {
         arg_print_errors(stderr, argument_end, PROGRAM_NAME);
         fprintf(stderr, "Try using --help option for more information on calling arguments.\n");
-        exit(1);
+        exit(PINGER_EXITERROR_PARAMETERS);
     }
 
-    int code = main_pinger(argument_source_ip->sval[0],
-                           argument_destination_address->sval[0],
+    int code = main_pinger(argument_address_source->sval[0],
+                           argument_address_destination->sval[0],
                            argument_ttl->ival[0],
-                           argument_listen->count,
-                           argument_continuous->count);
+                           argument_listen->count);
     arg_freetable(arguments_table, sizeof(arguments_table) / sizeof(arguments_table[0]));
     return code;
 }
 
-int main_pinger(const char* source_address,
-                const char* destination_address,
+int main_pinger(const char* source_address_string,
+                const char* destination_address_string,
                 int ttl,
-                int listen,
-                int continuous) {
+                int listen) {
     if (ttl < 1) {
         ttl = 1;
     }
@@ -96,16 +104,76 @@ int main_pinger(const char* source_address,
         ttl = 255;
     }
 
-    if (source_address == NULL) {
-        source_address == "127.0.0.1";
-    }
-
     printf("Arguments:\n");
-    printf("  source: %s\n", source_address);
-    printf("  destination: %s\n", destination_address);
+    printf("  source: %s\n", source_address_string);
+    printf("  destination: %s\n", destination_address_string);
     printf("  TTL: %d\n", ttl);
     printf("  listen: %s\n", listen > 0 ? "yes" : "no");
-    printf("  continuous: %s\n", continuous > 0 ? "yes" : "no");
+
+    pid = getpid();
+    catcher_listen_all = listen;
+
+    struct sigaction catcher_action;
+    memset(&catcher_action, 0, sizeof(catcher_action));
+    catcher_action.sa_handler = &main_catcher;
+    sigaction(SIGALRM, &catcher_action, NULL);
+    sigaction(SIGINT, &catcher_action, NULL);
+
+    int sd = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
+    const int on = 1;
+    setsockopt(sd, SOL_SOCKET, SO_BROADCAST, &on, sizeof(on));
+    int size = 60 * 1024;
+    setsockopt(sd, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
+
+    setuid(getuid());
+
+    struct itimerval timer;
+    timer.it_value.tv_sec = 0;
+    timer.it_value.tv_usec = 1;
+    timer.it_interval.tv_sec = 1;
+    timer.it_interval.tv_usec = 0;
+    setitimer(ITIMER_REAL, &timer, NULL);
+
+    struct addrinfo *destination_addrinfo;
+    int error_code;
+    error_code = getaddrinfo(destination_address_string, NULL, NULL, &destination_addrinfo);
+    if (error_code != 0) {
+
+        printf("Destination address was not resolved: %s.\n", gai_strerror(error_code));
+        return PINGER_EXITERROR_DESTINATION_ADDRESS;
+    }
+    struct sockaddr_in destination_address;
+    memset(&destination_address, 0, sizeof(destination_address));
+    destination_address.sin_family = AF_INET;
+    destination_address.sin_addr = ((struct sockaddr_in*) destination_addrinfo->ai_addr)->sin_addr;
+    freeaddrinfo(destination_addrinfo);
+
+    struct sockaddr_in source_address;
+    memset(&source_address, 0, sizeof(source_address));
+    source_address.sin_family = AF_INET;
+    if (source_address_string != NULL) {
+        struct addrinfo *source_addrinfo;
+        error_code = getaddrinfo(source_address_string, NULL, NULL, &source_addrinfo);
+        if (error_code != 0) {
+
+            printf("Source address was not resolved: %s.\n", gai_strerror(error_code));
+            return PINGER_EXITERROR_SOURCE_ADDRESS;
+        }
+        source_address.sin_addr = ((struct sockaddr_in*) source_addrinfo->ai_addr)->sin_addr;
+        freeaddrinfo(source_addrinfo);
+    } else {
+        source_address.sin_addr.s_addr = INADDR_ANY;
+    }
+    
+    // inet_ntoa uses ONE string buffer
+    printf("Pinging (%s)", inet_ntoa(destination_address.sin_addr));
+    printf(" from (%s).\n", inet_ntoa(source_address.sin_addr));
+
+    //TODO:Everything else
 
     return 0;
+}
+
+void main_catcher() {
+    //TODO
 }
---
+++ semester-2/lab01/pinger.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   pinger.h
+ * Author: keritaf
+ *
+ * Created on February 19, 2013, 11:29 AM
+ */
+
+#ifndef PINGER_H
+#define	PINGER_H
+
+#define PINGER_EXITERROR_PARAMETERS 1
+#define PINGER_EXITERROR_DESTINATION_ADDRESS 2
+#define PINGER_EXITERROR_SOURCE_ADDRESS 3
+
+#endif	/* PINGER_H */
+
